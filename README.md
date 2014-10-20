6fabd2607d6cdbd50a37282fab6303db04dea9dd
gogoprog <gogoprog@gmail.com>
2014-10-20 19:53:00 +0200

    First commit, initial code

---
+++ README.md
@@ -0,0 +1,19 @@
+gengine-labyr
+=============
+
+The first game made using gengine.
+
+#### How to run?
+Prepare gengine submodule
+
+    git submodule init
+    git submodule update
+
+Activate the gengine environment
+
+    source gengine/bin/activate
+    
+Then go to a sample and run it
+
+    cd src/
+    gengine-run
---
+++ src/tiles.lua
@@ -0,0 +1,18 @@
+Tiles = {
+    {
+        file = "tile0",
+        validDirections = { false, false, false, false },
+        rotation = 0
+    },
+    {
+        file = "tile1",
+        validDirections = { false, true, false, true },
+        rotation = 0
+    },
+    {
+        file = "tile2",
+        validDirections = { true, false, false, true },
+        rotation = 0,
+        corner = true
+    }
+}
